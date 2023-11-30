f6f6f3ff0bbe892b50e3cf5051ed07ca1880e648
kabiru <mwangikabiru21@gmail.com>
2023-11-30 18:11:08 +0300

    feat : Added day1 to day31

---
+++ day30/go.mod
@@ -0,0 +1,3 @@
+module github.com/BrianMwangi21/aoc2023.git
+
+go 1.21.0
---
+++ day4/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readFile(filename string) []string {
+	var data []string
+
+	f, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		data = append(data, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
+func main() {
+	data := readFile("")
+	fmt.Println(data)
+}

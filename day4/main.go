8dc4334b334a8b7bcd9999e52fed09c8182a5e63
kabiru <mwangikabiru21@gmail.com>
2023-12-14 18:20:59 +0300

    Changed to maps, little time diff

--- day4/main.go
+++ day4/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"time"
 )
 
 func readFile(filename string) []string {
@@ -89,6 +90,7 @@ func partOne(data []string) int {
 }
 
 func partTwo(data []string) int {
+	now := time.Now()
 	var copies []string
 	sanitized := sanitizeData(data)
 
@@ -116,6 +118,38 @@ func partTwo(data []string) int {
 		idx++
 	}
 
+	fmt.Println("Time taken: ", time.Since(now))
+	return len(copies)
+}
+
+func partTwoWithMap(data []string) int {
+	now := time.Now()
+	match_map := make(map[string]int)
+	var copies []string
+	sanitized := sanitizeData(data)
+
+	for index, v := range sanitized {
+		match_map[v] = index
+	}
+
+	copies = append(copies, sanitized...)
+	idx := 0
+
+	for idx != len(copies) {
+		combo := strings.Split(copies[idx], " | ")
+		matched_numbers := getMatchedNumbers(combo)
+
+		if len(matched_numbers) > 0 {
+			idx_match := match_map[copies[idx]]
+			for count := range matched_numbers {
+				copies = append(copies, sanitized[idx_match+1+count])
+			}
+		}
+
+		idx++
+	}
+
+	fmt.Println("Time taken: ", time.Since(now))
 	return len(copies)
 }
 
@@ -124,4 +158,5 @@ func main() {
 
 	fmt.Println("Part One: ", partOne(data))
 	fmt.Println("Part Two: ", partTwo(data))
+	fmt.Println("Part Two With Map: ", partTwoWithMap(data))
 }

ce0bfadf125283f2303fd34ebc390c585da00d16
kabiru <mwangikabiru21@gmail.com>
2023-12-14 13:59:10 +0300

    Phew, on first try. But the brute force way. Need to optimize

--- day4/main.go
+++ day4/main.go
@@ -4,7 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func readFile(filename string) []string {
@@ -31,7 +34,94 @@ func readFile(filename string) []string {
 	return data
 }
 
+func sanitizeData(data []string) []string {
+	var result []string
+
+	for _, v := range data {
+		index := strings.Index(v, ":")
+		result = append(result, v[index+2:])
+	}
+
+	return result
+}
+
+func extractDigits(text string) []string {
+	pattern := `\b\d+`
+	re := regexp.MustCompile(pattern)
+	matches := re.FindAllString(text, -1)
+	return matches
+}
+
+func getMatchedNumbers(combo []string) []string {
+	var result []string
+	winning_numbers := extractDigits(combo[0])
+	sample_numbers := extractDigits(combo[1])
+
+	for _, w := range winning_numbers {
+		for _, s := range sample_numbers {
+			if w == s {
+				result = append(result, w)
+			}
+		}
+	}
+
+	return result
+}
+
+func partOne(data []string) int {
+	var result int
+	sanitized := sanitizeData(data)
+
+	for _, v := range sanitized {
+		combo := strings.Split(v, " | ")
+		matched_numbers := getMatchedNumbers(combo)
+
+		if len(matched_numbers) > 0 {
+			if len(matched_numbers) == 1 {
+				result += 1
+			} else {
+				result += int(math.Pow(float64(2), float64(len(matched_numbers)-1)))
+			}
+		}
+	}
+
+	return result
+}
+
+func partTwo(data []string) int {
+	var copies []string
+	sanitized := sanitizeData(data)
+
+	copies = append(copies, sanitized...)
+	idx := 0
+
+	for idx != len(copies) {
+		combo := strings.Split(copies[idx], " | ")
+		matched_numbers := getMatchedNumbers(combo)
+
+		if len(matched_numbers) > 0 {
+			idx_match := 0
+			for idx_inner := range sanitized {
+				if copies[idx] == sanitized[idx_inner] {
+					idx_match = idx_inner
+					break
+				}
+			}
+
+			for count := range matched_numbers {
+				copies = append(copies, sanitized[idx_match+1+count])
+			}
+		}
+
+		idx++
+	}
+
+	return len(copies)
+}
+
 func main() {
-	data := readFile("")
-	fmt.Println(data)
+	data := readFile("input.txt")
+
+	fmt.Println("Part One: ", partOne(data))
+	fmt.Println("Part Two: ", partTwo(data))
 }
